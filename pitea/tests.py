1137503621d0f68045ae6955b7ab398a1bcff28e
Rodrigo <rodgalle@UCM.es>
2025-01-30 10:20:39 +0100

    tests actualizado

--- pitea/tests.py
+++ pitea/tests.py
@@ -4,7 +4,7 @@ import os
 # Ruta del script a probar
 SCRIPT_PATH = "script_ejecucion.py"
 
-def test_ocultar_desocultar():
+def test_ocultar_desocultar_lsb():
     # Prueba de ocultar con cifrado AES y LSB
     command = [
         "python3", SCRIPT_PATH, "ocultar",
@@ -58,6 +58,59 @@ def test_ocultar_desocultar():
     os.remove("archivos_prueba/datos_desocultos.txt")
     os.remove("archivos_prueba/audio_salida.wav")
 
+def test_ocultar_desocultar_sstv():
+    # Prueba de ocultar con cifrado AES y SSTV
+    command = [
+        "python3", SCRIPT_PATH, "ocultar",
+        "--modo-cifrado", "aes",
+        "--modo-cifrado-imagen", "text",
+        "--modo-cifrado-audio", "sstv",
+        "--contraseña", "prueba123",
+        "-i", "archivos_prueba/prueba.txt", 
+        "-o", "archivos_prueba/sstv.wav",
+        "-v"
+    ]
+    result = subprocess.run(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        check=True
+    )
+    assert result.returncode == 0, f"Error en la prueba. Código de salida: {result.returncode}"
+
+    # Prueba de desocultar con cifrado AES y SSTV
+    command = [
+        "python3", SCRIPT_PATH, "desocultar",
+        "--modo-cifrado", "aes",
+        "--modo-cifrado-imagen", "sstv",
+        "--modo-cifrado-audio", "sstv",
+        "-i", "archivos_prueba/imagen_salida_sstv.png",
+        "-o", "archivos_prueba/datos_desocultos.txt",
+        "--contraseña", "prueba123",
+        "-v"
+    ]
+    result = subprocess.run(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        check=True
+    )
+
+    assert result.returncode == 0, f"Error en la prueba. Código de salida: {result.returncode}"
+    
+    # Validar que los archivos sean iguales
+    with open("archivos_prueba/prueba.txt", "rb") as file1, open("archivos_prueba/datos_desocultos.txt", "rb") as file2:
+        assert file1.read() == file2.read(), "Los archivos no son iguales"
+
+    print("✅ Prueba de ocultar y desocultar completada con éxito : \n El archivo prueba.txt es igual a datos_desocultos.txt \n")
+    
+    # Eliminar archivos generados
+    os.remove("archivos_prueba/datos_desocultos.txt")
+    os.remove("archivos_prueba/imagen_salida_sstv.png")
+    os.remove("archivos_prueba/sstv.wav")
+
 
 # Ejecutar los casos de prueba
 def run_tests():
@@ -67,7 +120,8 @@ def run_tests():
             print("Ejecutando pruebas... \n")
 
             print("🧪 Prueba de ocultar y desocultar con cifrado AES y LSB")
-            test_ocultar_desocultar()
+            test_ocultar_desocultar_lsb()
+            #test_ocultar_desocultar_sstv()
 
             print(" \n🎉 Todas las pruebas han pasado correctamente. \n")
         except AssertionError as error:
